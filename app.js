e82c18888376fea572958b9e0030a7a5942d7cee
Ethan Harsh <ethan@ethanharsh.com>
2021-05-03 15:08:32 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+
+//const authRoutes = require('./routes/authRoutes')
+const projectRoutes = require('./routes/projectRoutes');
+//const APIRoutes = require('./routes/APIRoutes');
+
+const app = express();
+//view engine
+app.set('view-engine', 'ejs');
+
+//middleware
+app.use(express.static('public'));
+
+
+//routes
+app.get('/', (req, res) => res.render('index.ejs'));
+//app.use(authRoutes);
+app.use('/projects', projectRoutes);
+//app.use('/api/v1', APIRoutes);
+module.exports = app;
---
+++ routes/projectRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router
+    .route('/forkify')
+    .get((req, res) => res.render('forkify/index.ejs'));
+
+router
+    .route('pig_dice')
+    .get((req, res) => res.render('pig_dice/index.ejs'));
+
+router
+    .route('/pig_dice/rules')
+    .get((req, res) => res.render('pig_dice/rules/index.ejs'));
+
+router
+    .route('/budgety')
+    .get((req, res) => res.render('budgety/index.ejs'));
+
+module.exports = router;
